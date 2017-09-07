92234f413489f6ab532a32175a3f4d5168ff871d
Gelei Chen <chen1474@purdue.edu>
2017-09-07 14:31:25 -0400

    rm import

--- hack_etherscan/hack_etherscan/settings.py
+++ hack_etherscan/hack_etherscan/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app
 
 # REDIS related settings
 REDIS_HOST = 'localhost'
